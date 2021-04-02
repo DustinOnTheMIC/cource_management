0a5951027709785b84672a6347049536a42ec701
Dustin <buiductinwork@gmail.com>
2021-04-02 19:31:32 +0700

    push new branch

---
+++ src/Components/TeacherProfile/CommentRate/CommentRate.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ItemComment from "../ItemComment/ItemComment";
+import './CommentRate.css'
+import CommentBox from '../CommentBox/CommentBox.jsx'
+
+class CommentRate extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row mb-5">
+          <div className="row mb-5 justify-content-center ">
+            <div className="col-md-8 col-12">
+              <div class="form-row d-flex align-items-center justify-content-center">
+                <CommentBox/>
+              </div>
+              <div className="show-comment">
+                <ItemComment />
+                <ItemComment />
+                <ItemComment />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CommentRate;
