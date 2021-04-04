7acc1c9a4afee21de1fc27e7331c43894dfa8d37
rs-sonnvt <sonnvt@runsystem.net>
2021-04-04 13:21:44 +0700

    connect api to show subject

--- src/Components/HomeBodyComponents/BodyBottom/index.jsx
+++ src/Components/HomeBodyComponents/BodyBottom/index.jsx
@@ -1,143 +1,68 @@
-import React, { Component } from 'react';
+import axios from "axios";
+import React, { Component } from "react";
+
+import * as API from "../../../env";
+import ItemSubject from "./ItemSubject";
 
 class index extends Component {
-    render() {
-        return (
-            <section className="ftco-section">
-                <div className="container-xl">
-                    <div className="row justify-content-center">
-                        <div className="col-md-8 heading-section text-center mb-5" data-aos="fade-up" data-aos-duration="1000"  > {/**/}
-                            <span className="subheading">Class</span>
-                            <h2 className="mb-4">Browse Our Online Subject</h2>
-                        </div>
-                    </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSubject: "",
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(API.API_SUBJECT)
+      .then((res) => {
+        this.setState({
+          dataSubject: res.data.data,
+        });
+        console.log(this.state.dataSubject);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-                    <div className="row g-lg-2 justify-content-center">
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200"
-                            data-aos-duration="1000">
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-paint-palette"></span></div>
-                                <div className="text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Art &amp; Design</h2>
-                                    <span className="col-5 py-0" className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100"
-                            data-aos-duration="1000">
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-design"></span></div>
-                                <div className="text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>App PC</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300"
-                            data-aos-duration="1000" >
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-computer-science"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Data Science</h2>
-                                    <span className="col-5 py-0">10 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400"
-                            data-aos-duration="1000" >
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-magnifying-glass"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>History &amp; Archeologic</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400"
-                            data-aos-duration="1000" >
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-engineer"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Web Designer</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400"
-                            data-aos-duration="1000" > {/* */}
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-algorithm"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Information Software</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100"
-                            data-aos-duration="1000" >
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-dumbbell"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Health &amp; Fitness</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200"
-                            data-aos-duration="1000" >
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-bullhorn"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Marketing</h2>
-                                    <span className="col-5 py-0">15 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300"
-                            data-aos-duration="1000" >{/* */}
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-vector"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Graphic Design</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400"
-                            data-aos-duration="1000">
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-musical-notes"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Music</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400"
-                            data-aos-duration="1000" >
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-calendar-1"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Business Administration</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-xl-2 text-center d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400"
-                            data-aos-duration="1000" >
-                            <a href="#this" className="services-2">
-                                <div className="icon"><span className="flaticon-coding"></span></div>
-                                <div className="text text d-flex flex-column-reverse justify-content-between align-items-center">
-                                    <h2>Web Management</h2>
-                                    <span className="col-5 py-0">25 Class</span>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
+  render() {
+    let { dataSubject } = this.state;
+    return (
+      <section className="ftco-section">
+        <div className="container-xl">
+          <div className="row justify-content-center">
+            <div
+              className="col-md-8 heading-section text-center mb-5"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+            >
+              {" "}
+              {/**/}
+              <span className="subheading">Class</span>
+              <h2 className="mb-4">Browse Our Online Subject</h2>
+            </div>
+          </div>
+
+          <div className="row g-lg-2 justify-content-center">
+            {this.showSubject(dataSubject)}
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  showSubject = (dataSubject) => {
+    let result = null;
+    if (dataSubject) {
+      dataSubject.map((item,index) => {
+        result = (
+          <ItemSubject key={index} idSubject={item.id_subject} imgSubject={item.image} nameSubject={item.name} />
         );
+      });
     }
+
+    return result;
+  };
 }
 
 export default index;
