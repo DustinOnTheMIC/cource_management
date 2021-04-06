3e4b5c7af02c06103405ceae4e473bce55b5be20
rs-sonnvt <sonnvt@runsystem.net>
2021-04-06 22:47:24 +0700

    connect api for all teacher

--- src/env.js
+++ src/env.js
@@ -1,4 +1,5 @@
 export const API_SUBJECT = 'https://quanlikhoahoc.herokuapp.com/api/v1/auth/home'
 export const API_CLASS = 'https://quanlikhoahoc.herokuapp.com/api/v1/auth/class'
-export const API_TEACHER = 'https://quanlikhoahoc.herokuapp.com/api/v1/auth/listClass'
+export const API_TEACHER = 'https://quanlikhoahoc.herokuapp.com/api/v1/auth/teacher'
+export const API_CLASS_OF_TEACHER = 'https://quanlikhoahoc.herokuapp.com/api/v1/auth/listClass'
 export const API_AUTHENTICATION = 'Bearer ' + localStorage.getItem('token')
