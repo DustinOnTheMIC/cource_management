46f4d48a9d8e580198bc0d906b8ead3a439fa645
Dustin <59592287+DustinOnTheMIC@users.noreply.github.com>
2021-06-12 19:01:06 +0700

    update chatbot

--- src/Components/ChatBot/share/suggestClass/SuggestClass.jsx
+++ src/Components/ChatBot/share/suggestClass/SuggestClass.jsx
@@ -13,63 +13,35 @@ class SuggestClass extends Component {
     }
 
     componentDidMount() {
-        console.log(this.props);
+
         const { steps } = this.props;
         const { math } = steps;
-        this.setState({ math: math.value });
+        this.setState({ math: math.value }, () => this.handleClick());
     }
 
-    handleClick( math ) {
-
-        switch (math) {
-            case 8:
-                this.setState({ goTo: "class/chatbot/all/3" });
-                break;
-            case 5:
-                this.setState({ goTo: "class/chatbot/all/2" });
-                break;
-            default:
-                this.setState({ goTo: "class/chatbot/all/1" });
-                break;
+    handleClick() {
+        const { math } = this.state;
+        
+        if(math >= 8) {
+            this.setState({ goTo: "class/chatbot/all/3" });
+        } else if(math >= 5) {
+            this.setState({ goTo: "class/chatbot/all/2" });
+        } else {
+            this.setState({ goTo: "class/chatbot/all/1" });
         }
 
         this.setState({ redirect: true }, () => window.location.reload() );
     }
 
     render() {
-        const { math } = this.state
         return (
-            <div className="row">
+            <div className="d-none">
                 { this.state.redirect ? (
                     <div>
                         <Redirect push to="/" />
                         <Redirect push to={this.state.goTo} />
                     </div>
                 ) : null }
-                
-  
-                { math >= 8 ? 
-                    <button 
-                        style={{color:"yellow"}} 
-                        onClick={e => this.handleClick(8)}>
-                            Click here to go to classes level 3
-                    </button>
-                
-                : math >= 5 ? 
-                    <button 
-                        style={{color:"yellow"}} 
-                        onClick={e => this.handleClick(5)}>
-                        Click here to go to classes level 2
-                    </button>
-                
-                :  math < 5 ?
-                    <button 
-                        style={{color:"yellow"}} 
-                        onClick={e => this.handleClick(4)}>
-                            Click here to go to classes level 1
-                    </button>
-                : null }
-
             </div>
         );
     }
